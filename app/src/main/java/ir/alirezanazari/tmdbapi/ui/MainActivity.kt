5d90f8d46af22f3788ac6d2ccb60d21d763c2d26
Alireza <a.alirezanazari@gmail.com>
2020-04-30 15:11:54 +0430

    Config project and add main activity

---
+++ app/src/main/java/ir/alirezanazari/tmdbapi/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package ir.alirezanazari.tmdbapi.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.alirezanazari.tmdbapi.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
